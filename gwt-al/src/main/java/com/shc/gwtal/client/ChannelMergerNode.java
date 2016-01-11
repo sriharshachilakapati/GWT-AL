b342c38cc902e19b547df6aca0a5fe8734729f55
Sri Harsha Chilakapati <sriharshachilakapati@gmail.com>
2016-01-11 11:35:24 +0530

    Add ChannelMergerNode and ChannelSplitterNode

---
+++ gwt-al/src/main/java/com/shc/gwtal/client/ChannelMergerNode.java
@@ -0,0 +1,11 @@
+package com.shc.gwtal.client;
+
+/**
+ * @author Sri Harsha Chilakapati
+ */
+public class ChannelMergerNode extends AudioNode
+{
+    protected ChannelMergerNode()
+    {
+    }
+}
---
+++ gwt-al/src/main/java/com/shc/gwtal/client/ChannelSplitterNode.java
@@ -0,0 +1,11 @@
+package com.shc.gwtal.client;
+
+/**
+ * @author Sri Harsha Chilakapati
+ */
+public class ChannelSplitterNode extends AudioNode
+{
+    protected ChannelSplitterNode()
+    {
+    }
+}
