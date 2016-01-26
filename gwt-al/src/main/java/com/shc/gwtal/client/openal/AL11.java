7a5cb2cc9d49c65958dcce95f18a9b7aaba9c413
Sri Harsha Chilakapati <sriharshachilakapati@gmail.com>
2016-01-26 08:35:41 +0530

    Added OpenAL 1.1 constants in AL11 class

---
+++ gwt-al/src/main/java/com/shc/gwtal/client/openal/AL11.java
@@ -0,0 +1,28 @@
+package com.shc.gwtal.client.openal;
+
+/**
+ * @author Sri Harsha Chilakapati
+ */
+public final class AL11
+{
+    /**
+     * General tokens.
+     */
+    public static final int
+            AL_SEC_OFFSET                = 0x1024,
+            AL_SAMPLE_OFFSET             = 0x1025,
+            AL_BYTE_OFFSET               = 0x1026,
+            AL_STATIC                    = 0x1028,
+            AL_STREAMING                 = 0x1029,
+            AL_UNDETERMINED              = 0x1030,
+            AL_ILLEGAL_COMMAND           = 0xA004,
+            AL_SPEED_OF_SOUND            = 0xC003,
+            AL_LINEAR_DISTANCE           = 0xD003,
+            AL_LINEAR_DISTANCE_CLAMPED   = 0xD004,
+            AL_EXPONENT_DISTANCE         = 0xD005,
+            AL_EXPONENT_DISTANCE_CLAMPED = 0xD006;
+
+    private AL11()
+    {
+    }
+}
