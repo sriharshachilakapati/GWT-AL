135ca2b21e351c8d00c4a195081f9e9eceeda5c3
Sri Harsha Chilakapati <sriharshachilakapati@gmail.com>
2016-01-31 08:18:01 +0530

    Added EXTFloat32 class for constants

---
+++ gwt-al/src/main/java/com/shc/gwtal/client/openal/EXTFloat32.java
@@ -0,0 +1,14 @@
+package com.shc.gwtal.client.openal;
+
+/**
+ * @author Sri Harsha Chilakapati
+ */
+public final class EXTFloat32
+{
+    public static final int AL_FORMAT_MONO_FLOAT32   = 0x10010;
+    public static final int AL_FORMAT_STEREO_FLOAT32 = 0x10011;
+
+    private EXTFloat32()
+    {
+    }
+}
