ae09ba33fe62ef0017d75679213863c7577d732d
Sri Harsha Chilakapati <sriharshachilakapati@gmail.com>
2016-02-11 21:07:50 +0530

    Refactor BufferManager class

--- gwt-al/src/main/java/com/shc/gwtal/client/openal/BufferManager.java
+++ gwt-al/src/main/java/com/shc/gwtal/client/openal/BufferManager.java
@@ -3,23 +3,30 @@ package com.shc.gwtal.client.openal;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.shc.gwtal.client.openal.AL10.*;
+
 /**
  * @author Sri Harsha Chilakapati
  */
 final class BufferManager
 {
-    private Map<Integer, ALBuffer> managers = new HashMap<>();
+    private Map<Integer, ALBuffer> buffers = new HashMap<>();
+
+    BufferManager()
+    {
+        buffers.put(AL_NONE, new ALBuffer()); // AL_NONE is a valid buffer
+    }
 
     private int nextBufferID = 1;
 
     public ALBuffer getBuffer(int bufferID)
     {
-        return managers.get(bufferID);
+        return buffers.get(bufferID);
     }
 
     public int createBuffer()
     {
-        managers.put(nextBufferID, new ALBuffer());
+        buffers.put(nextBufferID, new ALBuffer());
         return nextBufferID++;
     }
 
@@ -30,6 +37,6 @@ final class BufferManager
 
     public void deleteBuffer(int bufferID)
     {
-        managers.remove(bufferID);
+        buffers.remove(bufferID);
     }
 }
