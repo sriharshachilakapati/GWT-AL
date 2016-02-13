de1f1cdf71b847a7f962c12f1b5215ced71d5b49
Sri Harsha Chilakapati <sriharshachilakapati@gmail.com>
2016-02-13 09:42:29 +0530

    Updated to include OpenAL example

--- examples/src/main/java/com/shc/gwtal/examples/client/Main.java
+++ examples/src/main/java/com/shc/gwtal/examples/client/Main.java
@@ -2,118 +2,161 @@ package com.shc.gwtal.examples.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.typedarrays.shared.ArrayBuffer;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.xhr.client.XMLHttpRequest;
+import com.shc.gwtal.client.openal.AL;
+import com.shc.gwtal.client.openal.AL10;
+import com.shc.gwtal.client.openal.ALContext;
 import com.shc.gwtal.client.openal.AudioDecoder;
 import com.shc.gwtal.client.webaudio.AudioBuffer;
-import com.shc.gwtal.client.webaudio.nodes.AudioBufferSourceNode;
 import com.shc.gwtal.client.webaudio.AudioContext;
 import com.shc.gwtal.client.webaudio.AudioContextException;
+import com.shc.gwtal.client.webaudio.nodes.AudioBufferSourceNode;
 
 public class Main implements EntryPoint
 {
-    private AudioBuffer           buffer;
-    private AudioBufferSourceNode source;
+    private AudioBuffer           webBuffer;
+    private AudioBufferSourceNode sourceNode;
 
     @Override
     public void onModuleLoad()
     {
-        try
+        // Test media support in browser
+        GWT.log("MP3: " + AudioDecoder.isSupported(AudioDecoder.FileFormat.MP3));
+        GWT.log("WAV: " + AudioDecoder.isSupported(AudioDecoder.FileFormat.WAV));
+        GWT.log("OGG: " + AudioDecoder.isSupported(AudioDecoder.FileFormat.OGG));
+        GWT.log("WEBM: " + AudioDecoder.isSupported(AudioDecoder.FileFormat.WEBM));
+
+        String url = "https://upload.wikimedia.org/wikipedia/commons/1/1d/Demo_chorus.ogg";
+
+        // Create an XMLHttpRequest to load the file from URL
+        XMLHttpRequest request = XMLHttpRequest.create();
+        request.setResponseType(XMLHttpRequest.ResponseType.ArrayBuffer);
+
+        request.open("GET", url);
+
+        // Attach a state change listener to check for the loading of the file
+        request.setOnReadyStateChange((r) ->
         {
-            // Test media support in browser
-            GWT.log("MP3: " + AudioDecoder.isSupported(AudioDecoder.FileFormat.MP3));
-            GWT.log("WAV: " + AudioDecoder.isSupported(AudioDecoder.FileFormat.WAV));
-            GWT.log("OGG: " + AudioDecoder.isSupported(AudioDecoder.FileFormat.OGG));
-            GWT.log("WEBM: " + AudioDecoder.isSupported(AudioDecoder.FileFormat.WEBM));
-
-            // Create an audio context
-            AudioContext context = AudioContext.create();
-
-            context.setOnStateChange(() ->
+            if (request.getReadyState() == XMLHttpRequest.DONE)
             {
-                GWT.log(context.getState().getJsState());
-            });
-
-            FlowPanel panel = new FlowPanel();
-
-            // Create the play button
-            Button playButton = new Button("Play");
-            playButton.addClickHandler((e) ->
-            {
-                // Stop if already playing
-                if (source != null)
+                try
                 {
-                    source.stop();
-                    source = null;
+                    createWebAudioAPIExample(request.getResponseArrayBuffer());
+                    createOpenALExample(request.getResponseArrayBuffer());
                 }
-
-                // Create the source node
-                source = context.createBufferSource();
-                source.setBuffer(buffer);
-
-                // Connect to the destination and play
-                source.connect(context.getDestination());
-                source.start();
-            });
-            playButton.setEnabled(false);
-
-            // Create the stop button
-            Button stopButton = new Button("Stop");
-            stopButton.addClickHandler((e) ->
-            {
-                if (source != null)
+                catch (AudioContextException e)
                 {
-                    source.stop();
-                    source = null;
+                    e.printStackTrace();
                 }
-            });
+            }
+        });
 
-            // Add the buttons to the panel
-            panel.add(playButton);
-            panel.add(stopButton);
+        // Send the request so browser starts loading the file
+        request.send();
+    }
 
-            // Add the panel to the document
-            RootPanel.get().add(panel);
+    private void createOpenALExample(ArrayBuffer data) throws AudioContextException
+    {
+        ALContext context = ALContext.create();
+        AL.setCurrentContext(context);
 
-            String url = "https://upload.wikimedia.org/wikipedia/commons/1/1d/Demo_chorus.ogg";
+        int alSource = AL10.alGenSources();
 
-            // Create an XMLHttpRequest to load the file from URL
-            XMLHttpRequest request = XMLHttpRequest.create();
-            request.setResponseType(XMLHttpRequest.ResponseType.ArrayBuffer);
+        FlowPanel panel = new FlowPanel();
+        panel.add(new Label("OpenAL example: "));
 
-            request.open("GET", url);
+        Button playButton = new Button("Play");
+        playButton.addClickHandler(event -> AL10.alSourcePlay(alSource));
+        playButton.setEnabled(false);
 
-            // Attach a state change listener to check for the loading of the file
-            request.setOnReadyStateChange((r) ->
-            {
-                if (request.getReadyState() == XMLHttpRequest.DONE)
-                {
-                    // Start decoding the data into an AudioBuffer
-                    context.decodeAudioData
-                            (
-                                    // The response that we got from the request
-                                    request.getResponseArrayBuffer(),
+        Button stopButton = new Button("Stop");
+        stopButton.addClickHandler(event -> AL10.alSourceStop(alSource));
 
-                                    // Upon success, we are returned with an AudioBuffer
-                                    (buffer) ->
-                                    {
-                                        // Save the buffer
-                                        this.buffer = buffer;
-                                        playButton.setEnabled(true);
-                                    }
-                            );
-                }
-            });
+        Button pauseButton = new Button("Pause");
+        pauseButton.addClickHandler(event -> AL10.alSourcePause(alSource));
 
-            // Send the request so browser starts loading the file
-            request.send();
-        }
-        catch (AudioContextException e)
+        panel.add(playButton);
+        panel.add(stopButton);
+        panel.add(pauseButton);
+
+        AudioDecoder.decodeAudio
+                (
+                        data,
+
+                        alBufferID ->
+                        {
+                            AL10.alSourcei(alSource, AL10.AL_BUFFER, alBufferID);
+                            playButton.setEnabled(true);
+                        },
+
+                        reason -> GWT.log("" + reason)
+                );
+
+        RootPanel.get().add(panel);
+    }
+
+    private void createWebAudioAPIExample(ArrayBuffer data) throws AudioContextException
+    {
+        // Create an audio context
+        AudioContext context = AudioContext.create();
+
+        context.setOnStateChange(() ->
         {
-            // In case of any exception, log to the console
-            GWT.log(e.toString());
-        }
+            GWT.log(context.getState().getJsState());
+        });
+
+        FlowPanel panel = new FlowPanel();
+
+        panel.add(new Label("WebAudioAPI example: "));
+
+        // Create the play button
+        Button playButton = new Button("Play");
+        playButton.addClickHandler((e) ->
+        {
+            // Stop if already playing
+            if (sourceNode != null)
+            {
+                sourceNode.stop();
+                sourceNode = null;
+            }
+
+            // Create the source node
+            sourceNode = context.createBufferSource();
+            sourceNode.setBuffer(webBuffer);
+
+            // Connect to the destination and play
+            sourceNode.connect(context.getDestination());
+            sourceNode.start();
+        });
+        playButton.setEnabled(false);
+
+        // Create the stop button
+        Button stopButton = new Button("Stop");
+        stopButton.addClickHandler((e) ->
+        {
+            if (sourceNode != null)
+            {
+                sourceNode.stop();
+                sourceNode = null;
+            }
+        });
+
+        // Add the buttons to the panel
+        panel.add(playButton);
+        panel.add(stopButton);
+
+        // Add the panel to the document
+        RootPanel.get().add(panel);
+
+        // Decode using the context
+        context.decodeAudioData(data, decodedData -> {
+            webBuffer = decodedData;
+            playButton.setEnabled(true);
+        });
     }
 }
